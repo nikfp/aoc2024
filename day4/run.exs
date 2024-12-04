5a8a210e3686d25ee99e6b9d1a83727660cbf027
nikfp <nik@nikfp.com>
2024-12-04 19:05:25 +0000

    added benchee

--- day4/run.exs
+++ day4/run.exs
@@ -1,5 +1,9 @@
 IO.puts("\nRunning script.....\n")
 
+Mix.install([
+  {:benchee, "~> 0.11", only: :dev}
+])
+
 Code.require_file("./lib/parser.ex", __DIR__)
 Code.require_file("./lib/part_1_solver.ex", __DIR__)
 Code.require_file("./lib/part_2_solver.ex", __DIR__)
@@ -7,6 +11,7 @@ Code.require_file("./lib/part_2_solver.ex", __DIR__)
 alias Parser
 alias Part1Solver
 alias Part2Solver
+alias Benchee
 
 test_input = Parser.parse("./inputs/test.txt")
 test_2_input = Parser.parse("./inputs/test2.txt")
@@ -16,7 +21,7 @@ test_input
 |> Part1Solver.solve()
 |> IO.inspect(label: "Part 1 test")
 
-prod_input
+Parser.parse("./inputs/prod.txt")
 |> Part1Solver.solve()
 |> IO.inspect(label: "Part 1 prod")
 
@@ -24,6 +29,18 @@ test_input
 |> Part2Solver.solve()
 |> IO.inspect(label: "Part 2 test")
 
-prod_input
+Parser.parse("./inputs/prod.txt")
 |> Part2Solver.solve()
 |> IO.inspect(label: "Part 2 prod")
+
+Benchee.run(%{
+   part1: fn -> 
+    Parser.parse("./inputs/prod.txt")
+    |> Part1Solver.solve()
+  end,
+  part2: fn -> 
+    Parser.parse("./inputs/prod.txt")
+    |> Part2Solver.solve()
+end
+}, parallel: 2)
+

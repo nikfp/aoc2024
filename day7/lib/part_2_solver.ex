1092611d1661f1b86575ca27aa369516306861b3
nikfp <nik@nikfp.com>
2024-12-09 19:24:56 +0000

    Solved day 7

--- day7/lib/part_2_solver.ex
+++ day7/lib/part_2_solver.ex
@@ -1,64 +1,9 @@
 defmodule Part2Solver do
   def solve(input) do
     input
-    |> Enum.drop(4)
-    |> Enum.take(1)
-    |> Enum.map(fn el -> can_be_true?(el) end)
-
-    # |> Enum.filter(fn line -> can_be_true?(line) end)
-    # |> Enum.map(& &1.target)
-    # |> Enum.sum()
-
-    # %{digits: digits, target: target} =
-    #   complex
-    #   |> Enum.drop(4)
-    #   |> List.first()
-    #
-    # IO.inspect(target)
-    #
-    # combinify_list(digits, 0, length(digits), target)
-
-    # complex_calculated =
-    #   complex
-    #   |> Enum.filter(fn %{digits: digits, target: target} ->
-    #     check_operator
-    #   end)
-
-    # [simple | complex_calculated]
-    # |> List.flatten()
-    # |> Enum.map(fn %{target: target} -> target end)
-    # |> Enum.sum()
-  end
-
-  defp combinify_list(list, join_index, list_length, target) do
-    if join_index + 1 >= list_length do
-      false
-    else
-      # get the start of the list up to the join index
-      head = Enum.take(list, join_index)
-
-      # get the join number
-      join =
-        Enum.drop(list, join_index)
-        |> Enum.take(2)
-        |> Enum.join("")
-        |> String.to_integer()
-
-      # get the tail of the list after join numbers
-
-      tail = Enum.drop(list, join_index + 2)
-
-      merged_list =
-        [head, join, tail]
-        |> List.flatten()
-        |> IO.inspect()
-
-      if(check_operator(merged_list, target)) do
-        true
-      else
-        combinify_list(list, join_index + 1, list_length, target)
-      end
-    end
+    |> Enum.filter(fn el -> can_be_true?(el) end)
+    |> Enum.map(& &1.target)
+    |> Enum.sum()
   end
 
   defp can_be_true?(line) do
@@ -72,13 +17,35 @@ defmodule Part2Solver do
   end
 
   defp check_operator([current_num | [next_num | rest_digits]], target) do
+    {match, value} =
+      tease_apart_numbers(target, current_num)
+
     check_operator([next_num | rest_digits], target - current_num) ||
-      check_operator([next_num | rest_digits], target / current_num) 
+      (rem(target, current_num) == 0 &&
+         check_operator([next_num | rest_digits], (target / current_num) |> trunc())) ||
+      (match == :match && check_operator([next_num | rest_digits], value))
   end
 
-  defp join_numbers(left, right) do
-    [left, right]
-    |> Enum.join()
-    |> String.to_integer()
+  defp tease_apart_numbers(target, current_num) do
+    target_str = Integer.to_string(target |> trunc()) |> String.split("", trim: true)
+
+    current_num_str =
+      Integer.to_string(current_num)
+      |> String.split("", trim: true)
+
+    trim_length = length(current_num_str)
+
+    trim = Enum.take(target_str, 0 - trim_length)
+
+    if trim == current_num_str do
+      return_str = target_str |> Enum.drop(0 - trim_length) |> Enum.join()
+
+      case return_str do
+        "" -> {:nomatch, 0}
+        val -> {:match, val |> String.to_integer()}
+      end
+    else
+      {:nomatch, 0}
+    end
   end
 end
--- day7/run.exs
+++ day7/run.exs
@@ -8,13 +8,13 @@ Code.require_file("./lib/part_1_solver.ex", __DIR__)
 
 alias Part1Solver
 
-# Parser.parse("./inputs/test.txt")
-# |> Part1Solver.solve()
-# |> IO.inspect(label: "test part 1", charlists: :as_integers)
-#
-# Parser.parse("./inputs/prod.txt")
-# |> Part1Solver.solve()
-# |> IO.inspect(label: "prod part 1", charlists: :as_integers)
+Parser.parse("./inputs/test.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "test part 1", charlists: :as_integers)
+
+Parser.parse("./inputs/prod.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "prod part 1", charlists: :as_integers)
 
 Code.require_file("./lib/part_2_solver.ex", __DIR__)
 
@@ -24,6 +24,6 @@ Parser.parse("./inputs/test.txt")
 |> Part2Solver.solve()
 |> IO.inspect(label: "test part 2", charlists: :as_integers)
 
-# Parser.parse("./inputs/prod.txt")
-# |> Part2Solver.solve()
-# |> IO.inspect(label: "prod part 2", charlists: :as_integers)
+Parser.parse("./inputs/prod.txt")
+|> Part2Solver.solve()
+|> IO.inspect(label: "prod part 2", charlists: :as_integers)

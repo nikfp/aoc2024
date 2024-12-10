2b38b7ae6c089b64bb77e20027395180a7faef81
nikfp <nik@nikfp.com>
2024-12-10 19:33:48 +0000

    finished day 8

--- day8/lib/part_2_solver.ex
+++ day8/lib/part_2_solver.ex
@@ -3,33 +3,59 @@ defmodule Part2Solver do
     %{antennae_map: antennae_map, upper_bound: upper_bound} = input
 
     Enum.reduce(antennae_map, %{}, fn {_key, [start | other_locs]}, acc ->
-      process_locations(start, other_locs, acc)
+      process_locations(start, other_locs, acc, upper_bound)
     end)
-    # |> Enum.filter(fn {{row, col}, _v} ->
-    #   row >= 0 && row < upper_bound &&
-    #     col >= 0 && col < upper_bound
-    # end)
-    # |> Enum.count()
+    |> Enum.count()
   end
 
-  def process_locations(_starting_point, [], result_map) do
+  def process_locations(_starting_point, [], result_map, _upper_bound) do
     result_map
   end
 
-  def process_locations(starting_point, other_locations, result_map) do
+  def process_locations(starting_point, other_locations, result_map, upper_bound) do
     antinodes_for_start =
       Enum.reduce(other_locations, result_map, fn loc, map ->
-        map
-        |> Map.put(locate_antinode(starting_point, loc), true)
-        |> Map.put(locate_antinode(loc, starting_point), true)
+        locate_antinodes(map, starting_point, loc, upper_bound)
       end)
 
     [new_start | new_locations] = other_locations
 
-    process_locations(new_start, new_locations, antinodes_for_start)
+    process_locations(new_start, new_locations, antinodes_for_start, upper_bound)
   end
 
-  def locate_antinode({row_a, col_a}, {row_b, col_b}) do
-    {row_a + (row_a - row_b), col_a + (col_a - col_b)}
+  def locate_antinodes(collector, {row_a, col_a}, {row_b, col_b}, upper_bound) do
+    row_change = row_a - row_b
+    col_change = col_a - col_b
+
+    locate_backwards(collector, {row_a, col_a}, row_change, col_change, upper_bound)
+    |> locate_forwards({row_a, col_a}, row_change, col_change, upper_bound)
+  end
+
+  defp locate_forwards(collector, {start_row, start_col}, row_change, col_change, upper_bound) do
+    if start_row >= upper_bound || start_row < 0 || start_col >= upper_bound || start_col < 0 do
+      collector
+    else
+      locate_forwards(
+        collector |> Map.put({start_row, start_col}, true),
+        {start_row + row_change, start_col + col_change},
+        row_change,
+        col_change,
+        upper_bound
+      )
+    end
+  end
+
+  defp locate_backwards(collector, {start_row, start_col}, row_change, col_change, upper_bound) do
+    if start_row >= upper_bound || start_row < 0 || start_col >= upper_bound || start_col < 0 do
+      collector
+    else
+      locate_backwards(
+        collector |> Map.put({start_row, start_col}, true),
+        {start_row - row_change, start_col - col_change},
+        row_change,
+        col_change,
+        upper_bound
+      )
+    end
   end
 end
--- day8/run.exs
+++ day8/run.exs
@@ -8,13 +8,13 @@ Code.require_file("./lib/part_1_solver.ex", __DIR__)
 
 alias Part1Solver
 
-# Parser.parse("./inputs/test.txt")
-# |> Part1Solver.solve()
-# |> IO.inspect(label: "test part 1")
-#
-# Parser.parse("./inputs/prod.txt")
-# |> Part1Solver.solve()
-# |> IO.inspect(label: "prod part 1")
+Parser.parse("./inputs/test.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "test part 1")
+
+Parser.parse("./inputs/prod.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "prod part 1")
 
 Code.require_file("./lib/part_2_solver.ex", __DIR__)
 
@@ -23,3 +23,7 @@ alias Part2Solver
 Parser.parse("./inputs/test.txt")
 |> Part2Solver.solve()
 |> IO.inspect(label: "test part 2")
+
+Parser.parse("./inputs/prod.txt")
+|> Part2Solver.solve()
+|> IO.inspect(label: "prod part 2")

7eaf4215db53cc1666754c9375ec08e67b0387af
nikfp <nik@nikfp.com>
2024-12-11 20:58:07 -0700

    working in day10p1

--- day10/lib/part_1_solver.ex
+++ day10/lib/part_1_solver.ex
@@ -3,8 +3,10 @@ defmodule Part1Solver do
     %{grid: grid, starts: starts, upper_bound: upper_bound} = input
 
     starts
+    # |> Enum.drop(3)
+    # |> Enum.take(1)
     |> Enum.flat_map(fn start ->
-      look_ahead(grid, 1, start)
+      look_ahead(grid, 1, start, %{})
     end)
     |> Enum.count()
   end
@@ -15,23 +17,24 @@ defmodule Part1Solver do
   # continue until the location is 9
   # when you get to the top, return something consistent
 
-  defp look_ahead(grid, 10, pos) do
-    [1]
+  defp look_ahead(grid, 10, pos, visited) do
+    [pos]
   end
 
-  defp look_ahead(grid, height, {row, col}) do
-    locations =
-      [
-        {row - 1, col},
-        {row + 1, col},
-        {row, col - 1},
-        {row, col + 1}
-      ]
-      |> Enum.filter(fn loc ->
-        Map.get(grid, loc, 0) == height
-      end)
-      |> Enum.flat_map(fn loc ->
-        look_ahead(grid, height + 1, loc)
-      end)
+  defp look_ahead(grid, height, {row, col}, visited) do
+
+    [
+      {row - 1, col},
+      {row + 1, col},
+      {row, col - 1},
+      {row, col + 1}
+    ]
+    |> Enum.filter(fn loc ->
+      Map.get(grid, loc, 0) == height 
+    end)
+    |> IO.inspect()
+    |> Enum.flat_map(fn loc ->
+      look_ahead(grid, height + 1, loc, visited)
+    end)
   end
 end

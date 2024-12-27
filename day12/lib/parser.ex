61bc253f568a53b188122bfbab195c602d0c49c9
nikfp <nik@nikfp.com>
2024-12-27 22:55:40 +0000

    day 12 p1 - made a plan

--- day12/lib/parser.ex
+++ day12/lib/parser.ex
@@ -1,5 +1,25 @@
 defmodule Parser do
   def parse(file) do
-    File.read!(file)
+    lines =
+      File.read!(file)
+      |> String.split("\n", trim: true)
+
+    upper_bound = length(lines)
+
+    lines =
+      lines
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, fn {line, row}, acc ->
+        String.split(line, "", trim: true)
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn {char, col}, inner_acc ->
+          Map.put(inner_acc, {row, col}, %{
+            char: char,
+            group: nil
+          })
+        end)
+      end)
+
+    %{lines: lines, upper_bound: upper_bound}
   end
 end
--- day12/lib/part_1_solver.ex
+++ day12/lib/part_1_solver.ex
@@ -2,4 +2,31 @@ defmodule Part1Solver do
   def solve(input) do
     input
   end
+
+  # work through all locations
+  # when you reach a location that doesn't have a group,
+  # assign the next group number to it
+  # flood fill search all connected elements of the same type
+  # in that group. 
+
+  # once groups are numbered, areas are the number of elements
+  # in the group. 
+
+  # maybe perimeter of a group is everywhere it touches another group,
+  # plus anywhere it touches a boundary. Can this be calculated during
+  # the flood fill? o
+
+  # FLOOD FILL - 
+  # get position and map of all locations
+  # - look up, left, down, right
+  # - if found location is same character, add to list to look at
+  # - if found location is another character, add to boundary to group
+  # - once the location is done evaluating, add to map of evaluated locations
+  # - recurse on to-evaluate list until empty
+
+  # FINAL CALC
+  # process all locations to groupings, 
+  # count members of group for area, 
+  # count perimeter locations noted for each location,
+  # multiply, then sum the total of all
 end

d6c85929f073b37810f23bdd3eb01c4a6f7ccda7
nikfp <nik@nikfp.com>
2024-12-27 22:13:56 +0000

    set up day 12

---
+++ day12/lib/part_1_solver.ex
@@ -0,0 +1,5 @@
+defmodule Part1Solver do
+  def solve(input) do
+    input
+  end
+end
---
+++ day12/run.exs
@@ -0,0 +1,18 @@
+IO.inspect("\nRunning script.....\n")
+
+Code.require_file("./lib/parser.ex", __DIR__)
+Code.require_file("./lib/part_1_solver.ex", __DIR__)
+
+alias Parser
+alias Part1Solver
+
+Parser.parse("./inputs/test_a.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "test part 1, test a, expects 140")
+
+Parser.parse("./inputs/test_b.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "test part 1, test b, expects 772")
+Parser.parse("./inputs/test_c.txt")
+|> Part1Solver.solve()
+|> IO.inspect(label: "test part 1, test c, expects 1930")

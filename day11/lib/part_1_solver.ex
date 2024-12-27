9eed92ff9812ed76b2e9e09e4021bdc3e67d8916
nikfp <nik@nikfp.com>
2024-12-27 20:10:36 +0000

    cleanups

--- day11/lib/part_1_solver.ex
+++ day11/lib/part_1_solver.ex
@@ -1,7 +1,5 @@
 defmodule Part1Solver do
   def solve(input, count_value) do
-    Counter.start_link(0)
-
     {counter, _} =
       input
       |> Enum.reduce({0, %{}}, fn el, {counter, cache} ->
@@ -67,37 +65,5 @@ defmodule Part1Solver do
         {value, cache}
     end
   end
-
-  # defp get_from_cache(cache, number, counter) do
-  #   Map.get_and_update(cache, {number, counter}, fn val ->
-  #     case val do
-  #       nil ->
-  #         number = check_number(number, integer)
-  #         {check_number(number, counter, cache)}
-  #
-  #       val ->
-  #         {val, cache}
-  #     end
-  #   end)
-  # end
 end
 
-defmodule Counter do
-  use Agent
-
-  def start_link(initial_value) do
-    Agent.start_link(fn -> initial_value end, name: __MODULE__)
-  end
-
-  def value do
-    Agent.get(__MODULE__, & &1)
-  end
-
-  def increment do
-    Agent.update(__MODULE__, &(&1 + 1))
-  end
-
-  def reset do
-    Agent.update(__MODULE__, fn _ -> 0 end)
-  end
-end

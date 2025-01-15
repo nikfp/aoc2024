8900ff989c6567c3b1141dc735e887370969f9df
nikfp <nik@nikfp.com>
2025-01-14 21:09:32 -0700

    finally solved d12p1 again

--- day12/lib/part_1_solver.ex
+++ day12/lib/part_1_solver.ex
@@ -5,9 +5,6 @@ defmodule Part1Solver do
     # Take the grid keys
     {updated_grid, _} =
       Map.keys(grid)
-      |> Enum.drop(5)
-      |> Enum.take(1)
-      |> IO.inspect()
       # Work through each one in a reducer
       |> Enum.reduce({grid, 0}, fn element, {new_grid, group_number} ->
         # Pull the location out of the grid
@@ -24,17 +21,17 @@ defmodule Part1Solver do
       end)
 
     updated_grid
-    |> IO.inspect()
     |> Enum.map(fn {_, v} -> v end)
     |> Enum.group_by(fn el -> el.group end)
-    |> Enum.map(fn {_, v} -> 
+    |> Enum.map(fn {_, v} ->
       area = length(v)
-    
-      perimeter = 
-        Enum.flat_map(v, fn %{boundaries: bounds} -> bounds end)
-        |> Enum.uniq()
+
+      perimeter =
+        Enum.flat_map(v, fn %{boundaries: bounds} ->
+          bounds |> Enum.uniq()
+        end)
         |> Enum.count()
-    
+
       area * perimeter
     end)
     |> Enum.sum()
@@ -60,7 +57,7 @@ defmodule Part1Solver do
     # Get surrounding locations as a list 
     {updated_lookup_locs, updated_location_details} =
       [
-        {row, col - 1}, 
+        {row, col - 1},
         {row, col + 1},
         {row - 1, col},
         {row + 1, col}
